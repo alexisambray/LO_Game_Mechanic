e9a9aaedc83ccba31d3ed196f21907aeba770182
alexisambray <alexisambray@gmail.com>
2024-10-20 15:03:38 +0800

    updated GameManager

--- Assets/_Script/GameManager.cs
+++ Assets/_Script/GameManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
-using UnityEditorInternal;
+// using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
